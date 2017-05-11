77ac5bceeb16fcbed125e26d938b5ddd54c4d754
Tiago Marques <tiago.map.marques@gmail.com>
2017-05-11 16:44:16 +0100

    Minor text edit

--- src/app/containers/about/about.container.js
+++ src/app/containers/about/about.container.js
@@ -9,8 +9,7 @@ class AboutContainer extends Container {
     return (
       <div>
         <p>React Seed is a starter project that implements best practices in coding, building and testing React apps.</p>
-        <p>This project is based on the Yeoman <a href="https://github.com/stylesuxx/generator-react-webpack-redux">React-Webpack-Redux generator</a> and inspired by <a href="https://github.com/mgechev/angular-seed">Angular 2 Seed</a> (a project by <a href="https://github.com/mgechev">Minko Gechev</a>).</p>
-        <p>This project is a sibling of the <a href="https://github.com/tiagomapmarques/angular-seed">Agular Seed</a> project</p>
+        <p>It is a sibling of the <a href="https://github.com/tiagomapmarques/angular-seed">Agular Seed</a> project</p>
 
         <h3>Features</h3>
         <ul>
