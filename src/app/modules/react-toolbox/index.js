a8ee0c5b46742b8c8910e223abb0a2b1534bc958
Tiago Marques <tiago.map.marques@gmail.com>
2017-05-12 00:15:10 +0100

    Fixed typo

--- src/app/modules/react-toolbox/index.js
+++ src/app/modules/react-toolbox/index.js
@@ -1,5 +1,5 @@
 
-// NOTE: to see if RadioButton bug is fixed, uncomment this line and comment the next one:
+// NOTE: to see if RadioGroup bug is fixed, uncomment this line and comment the next one:
 // export { RadioGroup } from 'react-toolbox/lib/real-radio';
 export { RadioGroup } from './radio-group.component';
 
