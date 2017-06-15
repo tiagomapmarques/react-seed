2fb066dfe82357c105b25e1f4842db1bcf385fa3
Tiago Marques <tiago.map.marques@gmail.com>
2017-06-15 01:55:56 +0100

    Added burger icon

--- src/app/types/icon/icon.type.ts
+++ src/app/types/icon/icon.type.ts
@@ -5,15 +5,17 @@ export enum IconType {
   HOME = 1,
   ABOUT,
   INFO,
+  BURGER,
 };
 
 export const ICON_MAP: BaseTypeMap = {
   'home': IconType.HOME,
   'info': [ IconType.INFO, IconType.ABOUT ],
+  'burger': IconType.BURGER,
 };
 
 export const ICON: BaseTypeFunctions = {
-  enumValues: (): IconType[] => [ IconType.HOME, IconType.ABOUT, IconType.INFO ],
+  enumValues: (): IconType[] => [ IconType.HOME, IconType.ABOUT, IconType.INFO, IconType.BURGER ],
   toModel: (icon: string): IconType => toType(icon, IconType),
   toJson: (icon: IconType): string => toString(icon, IconType),
   map: (icon: IconType): string => toMapped(icon, IconType, ICON_MAP),
